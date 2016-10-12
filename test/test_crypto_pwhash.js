6863e6391bc407abc1cd3a12766eec95e0765177
Rich Churcher <rich.churcher@gmail.com>
2016-10-12 14:47:32 +1300

    argon2i (non-str)

--- test/test_crypto_pwhash.js
+++ test/test_crypto_pwhash.js
@@ -46,6 +46,7 @@ describe('PWHash scryptsalsa208sha256', function() {
         var output = new Buffer(64);
         
         sodium.crypto_pwhash_scryptsalsa208sha256_ll(password, salt, N, r, p, output);
+        console.log(output.toString('base16'));
         assert(sodium.compare(result,output)==0);
         done(); 
     });
@@ -79,7 +80,7 @@ describe('PWHash', function() {
     });
 });
     
-describe('PWHash argon2i', function() {
+describe('PWHash argon2i_str', function() {
     it('should verify the generated hash with same password', function(done) {
         var password = new Buffer('this is a test password','utf8');
     
@@ -103,6 +104,28 @@ describe('PWHash argon2i', function() {
         
         assert(sodium.crypto_pwhash_argon2i_str_verify(out, badPassword)==false); 
         
+        done();
+    });
+});
+
+describe.only('PWHash argon2i', function() {
+    it('should verify the generated hash with same password using low level argon2i API', function(done) {
+        var password = new Buffer('this is a test password', 'utf8');
+        var salt = new Buffer('.SodiumChloride.', 'utf8');
+        var output = new Buffer(64);
+
+        var result = toBuffer('17ea6da95122e7e0344dab226838509b06dd3b6c2d3b05df360f88a09307915a8034a05a6bd68110b4f238b00088c12eccdbf0e25d99e26230959f0b296b24c1');
+
+        sodium.crypto_pwhash_argon2i(
+            output,
+            password,
+            salt,
+            sodium.crypto_pwhash_argon2i_OPSLIMIT_INTERACTIVE,
+            sodium.crypto_pwhash_argon2i_MEMLIMIT_INTERACTIVE,
+            sodium.crypto_pwhash_argon2i_ALG_ARGON2I13);
+
+        assert(sodium.compare(result, output)==0);
+
         done();
     });
 });
